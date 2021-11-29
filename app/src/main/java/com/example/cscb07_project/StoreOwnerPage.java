74083a9ab438e1ecf7b8ebe8accb74d3c6b90e88
muumijing <muumijing@gmail.com>
2021-11-29 16:42:11 -0500

    Update 11.29 by Yiru Jing

--- app/src/main/java/com/example/cscb07_project/StoreOwnerPage.java
+++ app/src/main/java/com/example/cscb07_project/StoreOwnerPage.java
@@ -1,18 +1,55 @@
 package com.example.cscb07_project;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ListView;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.TaskStackBuilder;
+import androidx.recyclerview.widget.RecyclerView;
 
-public class StoreOwnerPage extends AppCompatActivity {
+import java.util.List;
 
+import adapter.categoryAdapter;
 
+public class StoreOwnerPage extends AppCompatActivity implements View.OnClickListener{
+
+    private String userID;
+
+    private Store store;
+
+    RecyclerView categoryRecyclerView;
+
+    adapter.categoryAdapter categoryAdapter;
+    List<Category> categoryList;
+
+    TextView allCategory;
 
     @Override
     public void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.store_owner_page);
+        setContentView(R.layout.activity_store_page);
+        categoryRecyclerView = findViewById(R.id.categoryRecycler);
+
+        userID = getIntent().getStringExtra("userID");
+
     }
 }
+
+    private void addItem() {
+        Intent intent = new Intent(this, updateProductActivity.class);
+        // intent.putExtra("store", store);
+        intent.putExtra("store", store);
+        startActivity(intent);
+    }
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.btnAddItem:
+                addItem();
+                break;
+        }
+    }
